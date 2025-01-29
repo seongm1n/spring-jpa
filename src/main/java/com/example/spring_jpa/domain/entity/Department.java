1209c5a9ce4c40102bfdff5895e0bfa7a16fdf94
seongmin <0411tjdals34@gmail.com>
2025-01-29 21:46:58 +0900

    feat : 학과 엔티티 생성

---
+++ src/main/java/com/example/spring_jpa/domain/entity/Department.java
@@ -0,0 +1,33 @@
+package com.example.spring_jpa.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 10)
+    private String deptCode;
+
+    @Column(nullable = false, length = 10)
+    private String deptName;
+
+    @Column(length = 100)
+    private String location;
+
+    @OneToMany(mappedBy = "department")
+    private List<Student> students = new ArrayList<>();
+
+    @OneToMany(mappedBy = "department")
+    private List<Professor> professorList = new ArrayList<>();
+}
