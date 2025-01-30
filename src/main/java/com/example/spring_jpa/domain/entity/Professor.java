cb4a95063bc976d6f0a196fe353d9b9346a22083
seongmin <0411tjdals34@gmail.com>
2025-01-30 18:16:57 +0900

    feat : 교수 엔터티 생성

---
+++ src/main/java/com/example/spring_jpa/domain/entity/Professor.java
@@ -0,0 +1,34 @@
+package com.example.spring_jpa.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Professor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 10)
+    private String professorId;
+
+    @Column(nullable = false, length = 30)
+    private String name;
+
+    @Column(length = 20)
+    private String phone;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    @OneToMany(mappedBy = "professor")
+    private List<Course> courses = new ArrayList<>();
+}
